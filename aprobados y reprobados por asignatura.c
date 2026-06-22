a5e2503e4ce6f236a9bede6f1c2d3c713109a2a7
Alexa68888 <annet.paez@udla.edu.ec>
2026-06-21 19:21:28 -0500

    ingreso y validacion de calificaciones

--- aprobados y reprobados por asignatura.c
+++ aprobados y reprobados por asignatura.c
@@ -1,36 +1,16 @@
-#include <stdio.h>
-
-int main()
+void ingresarCalificaciones(float *cal, int n)
 {
-    int n;
-    printf("Ingrese el numero de estudiantes: ");
-    scanf("%d", &n);
-
-    float calificaciones[n][3];
-    int asignaturas = 3;
-
     for (int i = 0; i < n; i++)
     {
         printf("\nEstudiante %d:\n", i + 1);
-        for (int j = 0; j < asignaturas; j++)
+        for (int j = 0; j < ASIGNATURAS; j++)
         {
             do {
                 printf("  Asignatura %d: ", j + 1);
-                scanf("%f", &calificaciones[i][j]);
-                if (calificaciones[i][j] < 0 || calificaciones[i][j] > 10)
-                    printf("  Error: calificacion debe estar entre 0 y 10\n");
-            } while (calificaciones[i][j] < 0 || calificaciones[i][j] > 10);
+                scanf("%f", (cal + i * ASIGNATURAS + j));
+                if (*(cal + i * ASIGNATURAS + j) < 0 || *(cal + i * ASIGNATURAS + j) > 10)
+                    printf("  Error: debe estar entre 0 y 10\n");
+            } while (*(cal + i * ASIGNATURAS + j) < 0 || *(cal + i * ASIGNATURAS + j) > 10);
         }
     }
-
-    // Promedio por asignatura
-    printf("\n--- RESULTADOS POR ASIGNATURA ---\n");
-    for (int j = 0; j < asignaturas; j++)
-    {
-        float suma = 0;
-        for (int i = 0; i < n; i++)
-            suma += calificaciones[i][j];
-        printf("Asignatura %d -> Promedio: %.2f\n", j + 1, suma / n);
-    }
-    return 0;
 }
